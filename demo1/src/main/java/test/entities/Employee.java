86ed3e12cbde41b650b09d634990dad13adb6e4c
rahulkor <rahulkor@rahulkor.cybage.com>
2020-09-23 12:41:21 +0530

    toString added

--- demo1/src/main/java/test/entities/Employee.java
+++ demo1/src/main/java/test/entities/Employee.java
@@ -24,6 +24,11 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
 	
 	
 }
