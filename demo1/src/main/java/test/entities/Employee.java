3e66e973ec7d61eb99620f68c8d80f63ef9a047d
rahulkor <rahulkor@rahulkor.cybage.com>
2020-09-23 12:39:38 +0530

    getters ad setters added

--- demo1/src/main/java/test/entities/Employee.java
+++ demo1/src/main/java/test/entities/Employee.java
@@ -8,4 +8,22 @@ public class Employee {
 	public Employee() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
